d7684adf4d431f52a99f5ab1838a864d4836cbd8
Chaithanya Reddy <chaithanyareddysalagari@gmail.com>
2024-10-08 16:35:12 +0530

    Changed

--- obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+526d7420efbf47a48586df571caa55dc7ea25073")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
